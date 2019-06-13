445081a563c6d21320c15e78ffb9857a9b6cc22b
A Easterwood <aliciaeasterwood@gmail.com>
2019-06-13 15:04:57 -0500

    change webpack.config file name

--- webpack.common.js
+++ webpack.common.js
@@ -5,7 +5,7 @@ const merge = require('webpack-merge');
 const path = require('path');
 
 const SRC_DIR = path.join(__dirname, '/client/src');
-const DIST_DIR = path.join(__dirname, '/client/dist');
+const DIST_DIR = path.join(__dirname, '/client/dist'); //
 
 module.exports = merge(common, {
 	entry: `${SRC_DIR}/index.jsx`,
