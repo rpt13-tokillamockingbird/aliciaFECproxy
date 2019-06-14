ad436af499658a42fd6c891b636ecf62faeecf2d
A Easterwood <aliciaeasterwood@gmail.com>
2019-06-13 21:25:27 -0500

    add assets to component

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 import colehaan1 from '../assets/colehaan1.jpg';
+import colehaan2 from '../assets/colehaan2.jpg';
+import colehaan3 from '../assets/colehaan3.jpg';
+import colehaan4 from '../assets/colehaan4.jpg';
+import colehaan5 from '../assets/colehaan5.jpg';
 //import MainImg from './MainImg';
 //import { Discovery } from 'aws-sdk';
 
@@ -7,8 +11,11 @@ class App extends Component {
 	render () {
 		return (
 			<div className="App">
-				{/* <h5>Shoe Images</h5> */}
-				<img src="colehaan1" />
+				<img src={colehaan1} />
+				<img src={colehaan2} />
+				<img src={colehaan3} />
+				<img src={colehaan4} />
+				<img src={colehaan5} />
 			</div>
 		);
 	}
