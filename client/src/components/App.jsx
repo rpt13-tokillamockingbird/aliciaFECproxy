bfdc5e10ee8acbbe9edbb93f07bb3cd6ac96e45c
A Easterwood <aliciaeasterwood@gmail.com>
2019-06-13 19:24:05 -0500

    attempted to seperate components

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import colehaan1 from '../assets/colehaan1.jpg';
+//import MainImg from './MainImg';
 //import { Discovery } from 'aws-sdk';
 
 class App extends Component {
 	render () {
 		return (
-			<div>
-				<h5>Shoe Images</h5>
+			<div className="App">
+				{/* <h5>Shoe Images</h5> */}
+				<img src="colehaan1" />
 			</div>
 		);
 	}
---
+++ client/src/components/MainImg.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+//import { Discovery } from 'aws-sdk';
+
+class MainImg extends Component {
+	render () {
+		return (
+			<div className="MainImg">
+				<h5>Image Goes Here</h5>
+			</div>
+		);
+	}
+}
+
+export default MainImg;
